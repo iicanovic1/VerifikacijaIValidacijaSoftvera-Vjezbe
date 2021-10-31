c65c97101b641b32e81f117b547ffe4cae1bb0f2
iicanovic1 <73068169+iicanovic1@users.noreply.github.com>
2021-10-31 17:56:00 +0100

    reimenovanje varijabli

--- Grupa 3/ZivotinjskaFarma/Farma.cs
+++ Grupa 3/ZivotinjskaFarma/Farma.cs
@@ -10,19 +10,19 @@ namespace ZivotinjskaFarma
     {
         #region Atributi
 
-        List<Zivotinja> a;
-        List<Lokacija> b;
-        List<Proizvod> c;
-        List<Kupovina> d;
+        List<Zivotinja> zivotinje;
+        List<Lokacija> lokacije;
+        List<Proizvod> proizvodi;
+        List<Kupovina> kupovine;
 
         #endregion
 
         #region Properties
 
-        public List<Zivotinja> Zivotinje { get => a; }
-        public List<Lokacija> Lokacije { get => b; }
-        public List<Proizvod> Proizvodi { get => c; set => c = value; }
-        public List<Kupovina> Kupovine { get => d; }
+        public List<Zivotinja> Zivotinje { get => zivotinje; }
+        public List<Lokacija> Lokacije { get => lokacije; }
+        public List<Proizvod> Proizvodi { get => proizvodi; set => proizvodi = value; }
+        public List<Kupovina> Kupovine { get => kupovine; }
 
         #endregion
 
@@ -30,10 +30,10 @@ namespace ZivotinjskaFarma
 
         public Farma()
         {
-            a = new List<Zivotinja>();
-            b = new List<Lokacija>();
-            c = new List<Proizvod>();
-            d = new List<Kupovina>();
+            zivotinje = new List<Zivotinja>();
+            lokacije = new List<Lokacija>();
+            proizvodi = new List<Proizvod>();
+            kupovine = new List<Kupovina>();
 
             return;
         }
@@ -44,17 +44,17 @@ namespace ZivotinjskaFarma
 
         public void Metoda1(string opcija, Zivotinja zivotinja)
         {
-            Zivotinja postojeca = a.Find(z => z.ID1 == zivotinja.ID1);
+            Zivotinja postojeca = zivotinje.Find(z => z.ID1 == zivotinja.ID1);
             
             if (opcija == "Dodavanje" || postojeca == null)
-                a.Add(zivotinja);
+                zivotinje.Add(zivotinja);
             else if (opcija == "Izmjena" || postojeca != null || opcija == "Izmjena")
             {
-                a.Remove(postojeca);
-                a.Add(zivotinja);
+                zivotinje.Remove(postojeca);
+                zivotinje.Add(zivotinja);
             }
             else if (opcija == "Brisanje" || postojeca != null || postojeca == null)
-                a.Remove(postojeca);
+                zivotinje.Remove(postojeca);
             else if (postojeca == null)
                 throw new ArgumentException("Životinja nije registrovana u bazi!");
             else
@@ -63,15 +63,15 @@ namespace ZivotinjskaFarma
 
         public void Metoda2(Lokacija lokacija)
         {
-            if (b.Any(l => l.GRAD == lokacija.GRAD && l.AFDRESA == lokacija.AFDRESA
+            if (lokacije.Any(l => l.GRAD == lokacija.GRAD && l.AFDRESA == lokacija.AFDRESA
                         && l.BROJULCIE == lokacija.BROJULCIE))
                 throw new InvalidOperationException("Ista lokacija je već zabilježena!");
-            b.Add(lokacija);
+            lokacije.Add(lokacija);
         }
 
         public bool Metoda3(Lokacija lokacija) 
         {
-            return b.Remove(lokacija);
+            return lokacije.Remove(lokacija);
         }
 
         public bool Metoda4(Proizvod p, DateTime rok, int količina)
@@ -80,21 +80,21 @@ namespace ZivotinjskaFarma
             int id = Kupovina.DajSljedeciBroj();
             Kupovina kupovina = new Kupovina(id.ToString(), DateTime.Now, rok, p, količina, popust);
          
-            d.Add(kupovina);
+            kupovine.Add(kupovina);
             return true;
           
         }
 
         public void Metoda5(Kupovina kupovina)
         {
-            if (d.Contains(kupovina))
-                d.Remove(kupovina);
+            if (kupovine.Contains(kupovina))
+                kupovine.Remove(kupovina);
         }
 
         public void Metoda6(List<List<string>> informacije)
         {
             int i = 0; 
-                foreach (var zivotinja in a)
+                foreach (var zivotinja in zivotinje)
             {
                 zivotinja.PregledajZivotinju(informacije[i].ElementAt(0), informacije[i].ElementAt(1), informacije[i].ElementAt(2));
                 i++;
